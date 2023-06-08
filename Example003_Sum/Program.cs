c1a1bd812afb536bfa28a57c908d3b78652a16a3
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-08 18:08:14 +0300

    add Example003 Sum of two integer

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое целое число: ");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int numberB = Int32.Parse(Console.ReadLine());
+int summary = numberA + numberB;
+Console.WriteLine("Сумма введенных чисел равна " + summary);
