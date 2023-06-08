4730b9ca3cb81798aa69034d3a2cb47a149bb626
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-08 22:00:26 +0300

    add if_else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Уррааа, " + username.ToUpper());
+}
+else
+{
+    Console.WriteLine ("Привет, " + username);
+}
