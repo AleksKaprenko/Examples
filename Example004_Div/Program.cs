4d830491b2ef4fcdbd64b61ff81b4a74c452e432
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-24 16:43:53 +0300

    Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите первое число: ");
-double numberA = Double.Parse(Console.ReadLine());
+int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-double numberB = Double.Parse(Console.ReadLine());
-double result = numberA / numberB; // частное от деления двух чисел
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA / numberB; // частное от деления двух чисел
 Console.WriteLine("Частное от деления двух чисел равно: " + result);
