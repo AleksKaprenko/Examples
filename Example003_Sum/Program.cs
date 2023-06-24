9fe7128735f66cda175f650914d7c14842fc3270
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-24 16:40:24 +0300

    Summ_minor changes

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите первое целое число: ");
-int numberA = Int32.Parse(Console.ReadLine());
+int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число: ");
-int numberB = Int32.Parse(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
 int summary = numberA + numberB;
 Console.WriteLine("Сумма введенных чисел равна " + summary);
 Console.WriteLine();
